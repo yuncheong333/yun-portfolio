b40e4ca25ebf77f00479036e1eddb5e9ffc211d6
Cheong Yun <yuncheong3@gmail.com>
2025-04-30 01:29:16 +0900

    배포

--- backend/src/main/java/com/yuncheong/backend/controller/FileUploadController.java
+++ backend/src/main/java/com/yuncheong/backend/controller/FileUploadController.java
@@ -29,7 +29,7 @@ public class FileUploadController {
             String imageUrl = cloudinaryService.uploadImage(file);
 
             // 업로드된 이미지의 URL 반환
-            Map<String, String> result = Map.of("url", imageUrl);
+            Map<String, String> result = Map.of("secure_url", imageUrl);
             return ResponseEntity.ok(result);
         } catch (IOException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
