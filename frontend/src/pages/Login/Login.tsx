2f281c481904c51de5e8a447ae66f77112748e70
Cheong Yun <yuncheong3@gmail.com>
2025-04-29 16:49:01 +0900

    배포

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -3,12 +3,13 @@ import "../../styles/Login.css";
 import axios from "axios";
 
 import InputField from "../Login/components/InputField";
-
+import { useNavigate } from "react-router-dom";
 
 const Login: React.FC = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [rememberMe, setRememberMe] = useState(false);
+    const navigate = useNavigate(); // React Router v6 사용 시 페이지 이동 처리
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -20,9 +21,11 @@ const Login: React.FC = () => {
             });
 
             localStorage.setItem("accessToken", response.data.accessToken);
+            localStorage.setItem("refreshToken", response.data.refreshToken); // refreshToken 저장
             localStorage.setItem("adminId", response.data.adminId);
+
             alert("로그인 성공!");
-            window.location.href = "/";
+            navigate("/"); // 로그인 후 홈 페이지로 이동
         } catch (error) {
             alert("로그인 실패! 아이디 또는 비밀번호를 확인하세요.");
             console.error("로그인 오류:", error);
